cde094f3403980503aee5265e250c27b46e05bcc
Atul <ajayc5314@gmail.com>
2023-06-04 15:13:26 +0530

    changes done

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,14 @@
 const Product = require("../models/Schema")
 const getAllProducts = async(req,res) =>{
-    const myBookData = await Product.find({})
-    res.status(200).json({ myBookData});
+    const meeshoData = await Product.find({})
+    res.status(200).json({ meeshoData});
 
 };
 
 
 const getAllProductsTesting = async (req,res) =>{
-    const myBookData = await Product.find(req.query)
-    res.status(200).json({myBookData});
+    const  meeshoData= await Product.find(req.query)
+    res.status(200).json({ meeshoData});
 }
 
 module.exports = {getAllProducts,getAllProductsTesting}
